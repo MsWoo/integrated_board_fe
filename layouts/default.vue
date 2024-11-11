f73392cf78737d70767403fdce5529fcacbf85d6
MsWoo <minsoo010@naver.com>
2024-11-11 10:11:47 +0900

    quasar 적용 . quasar 의존성 추가 . default layout 추가 . PageTitle, PageDescription Component 추가

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>통합게시판</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container :style="pageContainerStyle">
+      <slot/>
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup lang="ts">
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl text-center">
+      <PageTitle title="Home"/>
+      <PageDescription description="Home Page입니다."/>
+    </div>
+  </q-page>
+</template>
